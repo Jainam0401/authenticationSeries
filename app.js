7cac233565e7c4108fa1aa56ad09c2770c446767
Jainam <jainammithalaljain@gmail.com>
2022-12-21 15:46:17 +0530

    Level 2 : environmental variable

---
+++ app.js
@@ -0,0 +1,77 @@
+//jshint esversion:6
+require('dotenv').config(); 
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption")
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb://localhost:27017/userDB")
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+// console.log(process.env.SECRET);
+
+userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields: ["password"]});
+
+const user = new mongoose.model("User",userSchema);
+
+app.get("/",(req,res)=>{
+    res.render("home");
+});
+
+app.get("/login",(req,res)=>{
+    res.render("login");
+});
+
+app.get("/register",(req,res)=>{
+    res.render("register");
+});
+
+app.post("/register",(req,res)=>{
+    const newUser = new user({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save((err)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.render("secrets")
+        }
+    })
+});
+
+app.post("/login",(req,res)=>{
+    
+        const username = req.body.username
+        const password=  req.body.password
+ 
+    user.findOne({email : username},(err,founduser)=> {
+        if(err){
+            res.send("wrong details");
+        }else{
+            if(founduser){
+                if(founduser.password === password){
+                    res.render("secrets");
+                }
+            }
+        }
+    })
+   
+});
+
+app.listen(3000,()=>{
+    console.log("Port Started on port 3000");
+})
